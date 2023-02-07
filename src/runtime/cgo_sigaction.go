5b06d6c1e4e06d9a80933a08dc93d8c5f0fdc856
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-07 17:36:21 -0500

    vdso and auxv

--- src/runtime/cgo_sigaction.go
+++ src/runtime/cgo_sigaction.go
@@ -4,7 +4,7 @@
 
 // Support for sanitizers. See runtime/cgo/sigaction.go.
 
-//go:build (linux && amd64) || (freebsd && amd64) || (linux && arm64) || (linux && ppc64le)
+//go:build (linux && amd64) || (freebsd && amd64) || (linux && arm64) || (linux && ppc64le) || (vxworks && amd64)
 
 package runtime
 
