b169fcbca28f4c37740da8bf7191279eefe8ff66
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-07 14:39:15 -0500

    vxworks syscall stubs

---
+++ builder.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# utility script used in process of porting golang to vxworks
+
+# build the bootstrap compiler
+# this version of go is built using the stock go compiler and recognizes
+# vxworks as a platform
+build_bootstrap() {
+    export GCFLAGS="all=-N -l" # disable optimizations
+    export GOROOT_BOOTSTRAP="" # which go compiler to use to build
+    export GOROOT="$(pwd)"
+    export GOTOOLSDIR="./pkg/" # where to install tools
+
+    INSTALL_DIR="$HOME/Temp/go_versions/vxworks"
+
+    cd src
+    ./make.bash
+    cd ..
+    cp -r bin "$INSTALL_DIR"
+    cp -r pkg "$INSTALL_DIR"
+    cp -r src "$INSTALL_DIR"
+}
+
+build_next() {
+    export GCFLAGS="" # disable optimizations
+    export GOROOT_BOOTSTRAP="$HOME/Temp/go_versions/vxworks" # which go compiler to use to build
+    export GOROOT="$(pwd)"
+    export GOOS="linux"
+    export GOARCH="amd64"
+
+    cd src
+    ./make.bash
+}
+
+case $1 in
+    bootstrap) build_bootstrap;;
+    next) build_next;;
+    *) echo "invalid operation";;
+esac
