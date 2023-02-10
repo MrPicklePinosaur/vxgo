5aade02c8c047acb5dac9609179ec9b56e9a2e14
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-09 21:17:39 -0500

    fix linker invalid relocation

--- src/cmd/link/internal/ld/elf.go
+++ src/cmd/link/internal/ld/elf.go
@@ -1784,6 +1784,9 @@ func asmbElf(ctxt *Link) {
 					interpreter = thearch.Linuxdynld
 				}
 
+			case objabi.Hvxworks:
+				interpreter = thearch.Linuxdynld
+
 			case objabi.Hfreebsd:
 				interpreter = thearch.Freebsddynld
 
@@ -2031,6 +2034,17 @@ func asmbElf(ctxt *Link) {
 		ph.Type = elf.PT_PAX_FLAGS
 		ph.Flags = 0x2a00 // mprotect, randexec, emutramp disabled
 		ph.Align = uint64(ctxt.Arch.RegSize)
+	} else if ctxt.HeadType == objabi.Hvxworks {
+		ph := newElfPhdr()
+		ph.Type = elf.PT_GNU_STACK
+		ph.Flags = elf.PF_W + elf.PF_R
+		ph.Align = uint64(ctxt.Arch.RegSize)
+
+		ph = newElfPhdr()
+		ph.Type = elf.PT_PAX_FLAGS
+		ph.Flags = 0x2a00 // mprotect, randexec, emutramp disabled
+		ph.Align = uint64(ctxt.Arch.RegSize)
+
 	} else if ctxt.HeadType == objabi.Hsolaris {
 		ph := newElfPhdr()
 		ph.Type = elf.PT_SUNWSTACK
